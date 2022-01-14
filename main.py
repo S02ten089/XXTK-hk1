bb855058bdb50f0734449f41fc531d12102fb2dd
tien vu <90943670+S02ten089@users.noreply.github.com>
2022-01-14 08:54:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+name = input("What is your name: ")
+S02 = set(name)
+print(S02, 'co do dai la', len(S02))
+Tuoi = input("Nhập số tuổi: ")
+lop = input("Nhập lớp: ")
+S02 = int(input("Nhập số nguyên dương(độ dài tên của mình) n = "))
+d = dict()
+for i in range(1, S02 + 1):
+    d[i] = i * i
+
+print(d)
